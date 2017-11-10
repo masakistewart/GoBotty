9b9d56440c487ea50885fe89c07bb2f92f44ffa1
localadmin <stewartcairo@gmail.com>
2017-11-10 05:57:23 -0500

    add logger via the wonderful William Kennedy Tutorial XD

---
+++ logging/logging.go
@@ -0,0 +1,31 @@
+package logging
+
+import (
+	"io"
+	"log"
+)
+
+var (
+	Trace 	*log.Logger
+	Error 	*log.Logger
+	Warning *log.Logger
+	Info	*log.Logger
+)
+
+func Init(traceHandle io.Writer, infoHandle io.Writer, warningHandle io.Writer, errorHandle io.Writer) {
+	Trace = log.New(traceHandle,
+		"Trace: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+		
+	Info = log.New(infoHandle,
+        "INFO: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Warning = log.New(warningHandle,
+        "WARNING: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Error = log.New(errorHandle,
+        "ERROR: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+}
--- server/server.go
+++ server/server.go
@@ -1,20 +1,26 @@
 package server
 
 import (
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/masakistewart/GoBotty/handlers"
 	"github.com/masakistewart/GoBotty/router"
+	. "github.com/masakistewart/GoBotty/logging"
 )
 
 func StartServer() {
 	// backingService holds all the variables needed in other parts of the app
 	// backingService := setup()
 
+	Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
 	r := router.New(handlers.Root)
 
 	r.Handle("GET", "/", handlers.Root)
 	r.Handle("GET", "/users", handlers.Users)
 	r.Handle("GET", "/users/:id", handlers.User)
 	http.ListenAndServe(":8080", r)
+
+	Trace.Println("Started the server on Port 8080")
 }
