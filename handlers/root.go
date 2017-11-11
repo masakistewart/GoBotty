8dab989657262b8d1bce623b6192007b9f75ff74
localadmin <stewartcairo@gmail.com>
2017-11-10 19:41:48 -0500

    builld out front end structure, folders etc

--- handlers/root.go
+++ handlers/root.go
@@ -1,10 +1,14 @@
 package handlers
 
 import "net/http"
-import "fmt"
 import "net/url"
+import "github.com/masakistewart/GoBotty/logging"
+import "fmt"
 
 func Root(w http.ResponseWriter, r *http.Request, params url.Values) {
-	
-	fmt.Fprint(w, "This is Root")
+	w.WriteHeader(200)
+	ip := r.Referer()
+	logging.Info.Printf("The root route was hit by %s", ip)
+	logging.Info.Println(r)
+	fmt.Fprintf(w,"IP: %s", ip)
 }
